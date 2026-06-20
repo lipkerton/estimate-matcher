c26523a5f39f08c4fa536fa387876e8657951cc9
lipkerton <petr.petuxof@yandex.ru>
2026-06-20 17:42:05 +0300

    pagination

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -95,6 +95,8 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 
 SPECTACULAR_SETTINGS = {
