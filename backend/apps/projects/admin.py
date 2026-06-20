a49d4dbe7e32e8ca0d19d1a1591862cb363eb191
lipkerton <petr.petuxof@yandex.ru>
2026-06-20 15:56:43 +0300

    projects.panel1

--- backend/apps/projects/admin.py
+++ backend/apps/projects/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from apps.projects.models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "created_at", "updated_at")
+    search_fields = ("name",)
