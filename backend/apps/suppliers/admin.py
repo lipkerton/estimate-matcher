c07c6cd739bb5287086c14d0dcb72f7187b68b29
lipkerton <petr.petuxof@yandex.ru>
2026-06-20 15:50:29 +0300

    suppliers.panel1

--- backend/apps/suppliers/admin.py
+++ backend/apps/suppliers/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from apps.suppliers import Supplier
+
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "inn", "currency", "created_at")
+    search_fields = ("name", "inn")
+    list_filter = ("currency",)
