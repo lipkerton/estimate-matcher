b730b316e1f0913cfb69b6c41dcf70caeb0bac87
lipkerton <petr.petuxof@yandex.ru>
2026-06-20 14:18:13 +0300

    projects.models1

--- backend/apps/projects/models.py
+++ backend/apps/projects/models.py
@@ -1,1 +1,18 @@
-# Create your models here.
+from django.db import models
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
+    
+    def __str__(self) -> str:
+        return self.name
