999b8fdc6041eb0b1acf7eb18bb0efc72de16a92
lipkerton <petr.petuxof@yandex.ru>
2026-06-20 16:00:26 +0300

    estimates.panel1

--- backend/apps/estimates/admin.py
+++ backend/apps/estimates/admin.py
@@ -1,1 +1,26 @@
-# Register your models here.
+from django.contrib import admin
+
+from apps.estimates.models import Estimate, EstimateItem
+
+
+@admin.register(Estimate)
+class EstimateAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "project", "created_at")
+    search_fields = ("name", "project__name")
+
+
+@admin.register(EstimateItem)
+class EstimateItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "estimate",
+        "raw_sku",
+        "raw_name",
+        "unit",
+        "quantity",
+        "product",
+        "matching_status",
+        "matching_confidence",
+    )
+    search_fields = ("raw_sku", "raw_name")
+    list_filter = ("matching_status", "unit")
