a3e89508fd0ecaf381a018bc797335f89d78e060
lipkerton <petr.petuxof@yandex.ru>
2026-06-20 17:50:10 +0300

    suppliers.serializers1 suppliers.views1

--- backend/apps/suppliers/views.py
+++ backend/apps/suppliers/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from rest_framework import filters, viewsets
+
+from apps.suppliers.models import Supplier
+from apps.suppliers.serializers import SupplierSerializer
+
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ("name", "inn")
+    ordering_fields = ("id", "name", "created_at", "updated_at")
+    ordering = ("name",)
